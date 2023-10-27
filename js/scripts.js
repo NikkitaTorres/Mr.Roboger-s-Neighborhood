9fbde9e2fbfe94a45a73821ec15d7162cf5a4a45
NikkitaTorres <nikkitatorres@yahoo.com>
2023-10-27 10:11:53 -0700

    Allow JS to pass 2nd test

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,12 @@
  function beepBoop(){
  // Push numbers from 0 to the user's inputted number into the array
  for (let i = 0; i <= num; i++) {
-  numbersArray.push(i);
+  
+  //Check if number contains the digit 1
+  if (i.toString().includes("1")) {
+    numbersArray.push("Beep!");
+    } else {
+      numbersArray.push(i);
+      }
   }
 }
